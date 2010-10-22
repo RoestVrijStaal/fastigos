b709d45a47f659b81e856bff8175f1836e936105
caragot <caragot@f5212048-a064-11de-bf0a-f72303de72c1>
2010-10-22 09:53:40 +0000

    Added simple version of null device

---
+++ src/drivers/null.c
@@ -0,0 +1,38 @@
+#include "null.h"
+
+int8_t null_init()
+{
+	debug_write_string("null_init() begin\n");
+	debug_write_string("null_init() end\n");
+	return 0;
+}
+
+int8_t null_deinit()
+{
+	debug_write_string("null_deinit() begin\n");
+	debug_write_string("null_deinit() end\n");
+	return 0;
+}
+
+int8_t null_read(int8_t * byte)
+{
+	debug_write_string("null_read( ptr: 0x");
+/*	debug_write_uint32(byte); @todo fix this */
+	debug_write_string(") begin\n");
+	byte = 0;
+	debug_write_string("null_read() end\n");
+	return 0;
+}
+
+int8_t null_write(int8_t byte)
+{
+	debug_write_string("null_write( ptr: 0x");
+	debug_write_uint32(byte);
+	debug_write_string(" val: ");
+/*	debug_write_uint8(*byte); @todo fix this */
+	debug_write_string(") begin\n");
+	byte = 0;
+	debug_write_string("null_write() end\n");
+	return 0;
+}
+
