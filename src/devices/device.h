ed2a56718e6d86e208e41c8d0342e69839dafa33
caragot <caragot@f5212048-a064-11de-bf0a-f72303de72c1>
2010-10-23 23:01:56 +0000

    VGA console driver working

--- src/devices/device.h
+++ src/devices/device.h
@@ -2,6 +2,7 @@
 #define __DEVICE_H__
 
 #include "../common/typedefs.h"
+#include "../debug/debug.h"
 
 #define __FOS_DEVICE_STATUS		int8_t
 #define __FOS_DEVICE_STATUS_OK 		 0
--- src/kernel/kernel.h
+++ src/kernel/kernel.h
@@ -7,8 +7,8 @@
 
 // devices
 #include "../devices/device.h"
-
 #include "../devices/null.h"
+#include "../devices/video/vga.h"
 
 __attribute__((noreturn)) void kernel_main();
 __attribute__((noreturn)) void kernel_die(char *message);
